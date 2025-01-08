efb12f5691d31febbdc39d259b4d1af53778ea84
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-08 11:28:44 +0500

    change from select to popover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,7 @@
 import { Link } from "react-router";
-import { Moon, ShoppingCart } from "lucide-react";
+import { ChevronDown, Moon, ShoppingCart } from "lucide-react";
 import { Separator } from "./ui/separator";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 
 export default function Header() {
   return (
@@ -21,29 +15,28 @@ export default function Header() {
             <Moon />
           </button>
           <Separator orientation="vertical" className="h-4 bg-zinc-100" />
-          <button
-            type="button"
-            className="relative flex items-center gap-[.375rem]"
-          >
+          <Link to="/" className="relative flex items-center gap-[.375rem]">
             <ShoppingCart className="shrink-0" />
             <p className="hidden min-[425px]:block">Cart (2)</p>
             <span className="absolute -right-2 -top-1 flex size-4 items-center justify-center rounded-full bg-purple-800 text-xs leading-none text-white min-[425px]:hidden">
               2
             </span>
-          </button>
+          </Link>
           <Separator orientation="vertical" className="h-4 bg-zinc-100" />
-          <Select>
-            <SelectTrigger>
-              <SelectValue
-                placeholder="Log In"
-                className="text-sm min-[425px]:text-base"
-              />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="login">Log In</SelectItem>
-              <SelectItem value="signup">Sign Up</SelectItem>
-            </SelectContent>
-          </Select>
+          <Popover modal>
+            <PopoverTrigger className="flex items-center gap-1">
+              <span className="text-sm min-[425px]:text-base">Log In</span>
+              <ChevronDown className="size-4" />
+            </PopoverTrigger>
+            <PopoverContent className="w-auto space-y-4 min-[425px]:w-52">
+              <Link to="/" className="block w-full text-left">
+                Log In
+              </Link>
+              <Link to="/" className="block w-full text-left">
+                Sign In
+              </Link>
+            </PopoverContent>
+          </Popover>
         </div>
       </div>
     </header>
