c6b3f8a3cf6b38b2d8ec2a3dde986f8cf8041dd3
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-10 01:29:43 +0500

    add sidebar trigger button

--- src/components/ProductsHeader.tsx
+++ src/components/ProductsHeader.tsx
@@ -5,13 +5,19 @@ import {
   SelectTrigger,
   SelectValue,
 } from "./ui/select";
+import { SidebarTrigger } from "./ui/sidebar";
 
 export default function ProductsHeader() {
   return (
     <div className="mb-[.875rem] flex items-center justify-between border-b border-purple-700 pb-4">
-      <h1 className="text-2xl font-semibold">All Products</h1>
+      <div className="flex items-center gap-x-[.625rem]">
+        <SidebarTrigger className="size-6 md:hidden [&_svg]:size-6" />
+        <h1 className="text-2xl font-semibold">All Products</h1>
+      </div>
       <div className="flex items-center gap-2">
-        <span className="text-sm uppercase tracking-widest">sort by</span>
+        <span className="hidden text-sm uppercase tracking-widest min-[425px]:block">
+          sort by
+        </span>
         <Select>
           <SelectTrigger className="gap-3 border border-zinc-300 p-2 text-sm">
             <SelectValue placeholder="Default" />
