67f0ec7cc1313afef1dba82046f7f89b4214f993
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-11 16:15:01 +0500

    add dynamic segment

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<AppLayout />}>
           <Route index element={<Home />} />
-          <Route path="product" element={<Product />} />
+          <Route path="product/:id" element={<Product />} />
         </Route>
       </Routes>
     </BrowserRouter>
