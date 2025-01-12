7dfed7ffb633047b9e31ad13b136bcc778b078ab
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-12 18:04:59 +0500

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { AppLayout } from "./app-layout";
 import { Home } from "./pages/home";
 import { Product } from "./pages/product";
+import { ThemeProvider } from "./components/ThemeProvider";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<AppLayout />}>
-          <Route index element={<Home />} />
-          <Route path="product/:id" element={<Product />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+            <Route path="product/:id" element={<Product />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
