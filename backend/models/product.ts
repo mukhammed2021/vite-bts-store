57b430dced374ed453374a366ad8e49c0b8ad42e
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-20 11:43:29 +0500

    update schema

--- backend/models/product.ts
+++ backend/models/product.ts
@@ -2,12 +2,15 @@ import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
-const productSchema = new Schema({
-  title: String,
-  price: Number,
-  category: String,
-  imageUrl: String,
-  exclusive: Boolean,
-});
+const productSchema = new Schema(
+  {
+    name: String,
+    price: Number,
+    category: String,
+    imageUrl: String,
+    exclusive: Boolean,
+  },
+  { versionKey: false }
+);
 
 export const Product = mongoose.model("Product", productSchema);
