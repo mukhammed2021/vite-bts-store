c428ca278bec17f4da81db0451b685e952b152a8
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-20 11:42:54 +0500

    connection to db

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import productRoutes from "./routes/product-routes";
 
-const PORT = 3000;
+dotenv.config({ path: ".env" });
 
 const app = express();
+app.use(express.json());
+app.use(productRoutes);
 
-app.listen(PORT, (err) => {
-  err ? console.log(err) : console.log(`Listening port ${PORT}`);
+async function connectToDb() {
+  try {
+    if (typeof process.env.ATLAS_URI === "undefined") {
+      throw new Error("Environment variable ATLAS_URI is undefined");
+    }
+
+    await mongoose.connect(process.env.ATLAS_URI);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(`DB connection error: ${err}`);
+  }
+}
+connectToDb();
+
+app.listen(process.env.PORT, (err) => {
+  err ? console.log(err) : console.log(`Listening port ${process.env.PORT}`);
 });
