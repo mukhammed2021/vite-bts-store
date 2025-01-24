f4cef97eacadb030240e4c4321860a08f0f22a2e
Mukhammed <massalimov.mukhammed@mail.ru>
2025-01-24 12:05:48 +0500

    configure multer

--- backend/routes/product-routes.ts
+++ backend/routes/product-routes.ts
@@ -1,9 +1,22 @@
 import express from "express";
-import { getProduct, getProducts } from "../controllers/product-controller";
+import multer from "multer";
+import { addProduct, getProduct, getProducts } from "../controllers/product-controller";
+
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "uploads/");
+  },
+  filename(req, file, callback) {
+    callback(null, new Date().toISOString().replace(/:/g, "-") + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/products/:id", getProduct);
+router.post("/products", upload.single("imageUrl"), addProduct);
 
 export default router;
