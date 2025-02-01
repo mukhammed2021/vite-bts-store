3ccde4cec387b4150505502d39cdb8738190a753
Mukhammed <massalimov.mukhammed@mail.ru>
2025-02-02 01:24:40 +0500

    update properties

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -1,35 +1,35 @@
 import { Link } from "react-router";
 
 interface ProductProps {
-  id: number;
-  src: string;
-  title: string;
+  _id: string;
+  imageUrl: string;
+  name: string;
   category: string;
   price: number;
   exclusive: boolean;
 }
 
 export default function Product({
-  id,
-  src,
-  title,
+  _id,
+  imageUrl,
+  name,
   category,
   price,
   exclusive,
 }: ProductProps) {
   return (
     <article className="group relative flex flex-col rounded-sm border border-zinc-200 dark:border-zinc-800">
-      <Link to={`/product/${id}`} className="block overflow-hidden">
+      <Link to={`/product/${_id}`} className="block overflow-hidden">
         <img
-          src={src}
-          alt={title}
+          src={`http://localhost:3000${imageUrl}`}
+          alt={name}
           className="aspect-square size-full rounded-t-sm object-cover transition-transform duration-300 group-hover:scale-110"
         />
       </Link>
       <div className="flex flex-auto flex-col px-2 pb-[.625rem] pt-2">
         <h3 className="mb-[1.125rem] grow text-sm font-semibold dark:text-zinc-400">
-          <Link to={`/product/${id}`} className="after:absolute after:inset-0">
-            {title}
+          <Link to={`/product/${_id}`} className="after:absolute after:inset-0">
+            {name}
           </Link>
         </h3>
         <div className="relative space-y-[.875rem] text-xs">
